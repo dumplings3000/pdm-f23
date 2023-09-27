ced47a0a2a96fdd9f1e6fabb0a0bc0bb64325d4e
sam_iscilab621 <sam8899yy.ee12@nycu.edu.tw>
2023-09-27 19:12:48 +0800

    dev

--- hw1/reconstruct.py
+++ hw1/reconstruct.py
@@ -3,17 +3,17 @@ import open3d as o3d
 import argparse
 import cv2
 
-pcd =[]
+pcd = []
 colors = []
 
-def depth_image_to_point_cloud(rgb, depth):
-     # Get point cloud from rgb and depth image 
 
-    #get rgb and depth images
+def depth_image_to_point_cloud(rgb, depth):
+    # Get point cloud from rgb and depth image
+
+    # get rgb and depth images
     scale = 1000
     rgb_image = cv2.imread(rgb)
-    depth_image = cv2.imread(depth,cv2.IMREAD_UNCHANGED).astype(float) / scale
-
+    depth_image = cv2.imread(depth, cv2.IMREAD_UNCHANGED).astype(float) / scale
 
     # camera internal parameter
     # fov = 90 degree and the camera resolution is 512 * 512,depth_scale = 1000
@@ -22,24 +22,27 @@ def depth_image_to_point_cloud(rgb, depth):
     cy = height/2
     fy = cy
     fx = cx
-    
+
     # pixels with depth to point cloud
     for i in range(height):
         for j in range(width):
             z = depth_image[i, j]
             y = (-i+cy) * z / fy
             x = (-j+cx) * z / fx
-            color = rgb_image[i,j]/255
-            pcd.append([x,y,z])
-            colors.append([color[2],color[1],color[0]])
-    return pcd,colors
+            color = rgb_image[i, j]/255
+            pcd.append([x, y, z])
+            colors.append([color[2], color[1], color[0]])
 
+    # create point cloud object
+    pcd_o3d = o3d.geometry.PointCloud() 
+    pcd_o3d.points = o3d.utility.Vector3dVector(pcd)
+    pcd_o3d.colors = o3d.utility.Vector3dVector(colors)
+
+    return pcd_o3d
 
 def preprocess_point_cloud(pcd, voxel_size):
-    # TODO: Do voxelization to reduce the number of points for less memory usage and speedup
-    raise NotImplementedError
+    pcd_down = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd, voxel_size=voxel_size)
     return pcd_down
-
 
 def execute_global_registration(source_down, target_down, source_fpfh,
                                 target_fpfh, voxel_size):
@@ -48,8 +51,13 @@ def execute_global_registration(source_down, target_down, source_fpfh,
 
 
 def local_icp_algorithm(source_down, target_down, trans_init, threshold):
-    # TODO: Use Open3D ICP function to implement
-    raise NotImplementedError
+    icp = o3d.pipelines.registration.registration_icp(
+        source_down, target_down, threshold, trans_init,
+        o3d.pipelines.registration.TransformationEstimationPointToPoint(),
+        o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=50))
+
+    # Run the ICP registration
+    result = icp.registration_result.transform
     return result
 
 
@@ -61,15 +69,15 @@ def my_local_icp_algorithm(source_down, target_down, trans_init, voxel_size):
 
 def reconstruct(args):
     # TODO: Return results
-    """
-    For example:
-        ...
-        args.version == 'open3d':
-            trans = local_icp_algorithm()
-        args.version == 'my_icp':
-            trans = my_local_icp_algorithm()
-        ...
-    """
+    # """
+    # For example:
+    #     ...
+    #     args.version == 'open3d':
+    #         trans = local_icp_algorithm()
+    #     args.version == 'my_icp':
+    #         trans = my_local_icp_algorithm()
+    #     ...
+    # """
     raise NotImplementedError
     return result_pcd, pred_cam_pos
 
@@ -77,15 +85,17 @@ def reconstruct(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--floor', type=int, default=1)
-    parser.add_argument('-v', '--version', type=str, default='my_icp', help='open3d or my_icp')
-    parser.add_argument('--data_root', type=str, default='data_collection/first_floor/')
+    parser.add_argument('-v', '--version', type=str,
+                        default='my_icp', help='open3d or my_icp')
+    parser.add_argument('--data_root', type=str,
+                        default='data_collection/first_floor/')
     args = parser.parse_args()
 
     if args.floor == 1:
         args.data_root = "data_collection/first_floor/"
     elif args.floor == 2:
         args.data_root = "data_collection/second_floor/"
-    
+
     # TODO: Output result point cloud and estimated camera pose
     '''
     Hint: Follow the steps on the spec
